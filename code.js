e6a4b7a2838c01c2dee3730cbb2b7e5d14b9ac97
Ian <ianm2927@gmail.com>
2021-11-11 16:48:57 -0500

    V1: v2

--- code.js
+++ code.js
@@ -2,13 +2,29 @@
 
 const container = document.querySelector('.container')
  const url = 'https://jsonplaceholder.typicode.com/users'
- let fragment = document.createDocumentFragment()
+ let fragment = document.createDocumentFragment();
+ const buscar = document.getElementById('search');
+ const btn = document.querySelector('.btn_buscar')
+
+
+ const deleteCom = str =>{      //Eliminar comillas 
+    str = str.replace(/["']/g, "");
+    return str
+  }
+
+  const parseJson = str =>{     //Parsear texto api
+      str = JSON.stringify(str)
+      return str
+  }
+
 
 
  const getData = async()=>{
      try{
          const datos = await axios(url)
          const data = datos.data
+         const search = await busqueda(data)
+         console.log(search)
          data.forEach(data => {
              console.log(data)
              const divP = document.createElement('div');
@@ -18,12 +34,11 @@ const container = document.querySelector('.container')
                  email: data.email,
                  adress: data.city
              }
-
              let jsonTxtName = parseJson(elemt.nombre);
              let clenTxt = deleteCom(jsonTxtName)
-             divP.textContent = clenTxt
-             fragment.appendChild(divP)
-             container.appendChild(fragment)   
+             divP.textContent = clenTxt;
+             fragment.appendChild(divP);
+             container.appendChild(fragment) ;  
          });
 
      }catch(e){
@@ -33,16 +48,10 @@ const container = document.querySelector('.container')
  }
 
 
- const deleteCom = str =>{
-    str = str.replace(/["']/g, "");
-    return str
-  }
-
-  const parseJson = str =>{
-      str = JSON.stringify(str)
-      return str
-  }
 
 
- getData()
+ 
+
+
+   
 
