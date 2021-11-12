2e3587c70ff5719547229f3bf62e3cae1720421b
Ian <ianm2927@gmail.com>
2021-11-12 17:31:08 -0500

    V1: v3

--- code.js
+++ code.js
@@ -1,10 +1,40 @@
 "use strict"
 
-const container = document.querySelector('.container')
  const url = 'https://jsonplaceholder.typicode.com/users'
  let fragment = document.createDocumentFragment();
- const buscar = document.getElementById('search');
- const btn = document.querySelector('.btn_buscar')
+
+
+ const getDataAPI = async ()=>{
+     try{
+         const datosApi = await axios(url);
+         const datos = datosApi.data
+         console.log(datos) 
+         const data = await showData(datos)
+         console.log(data)
+
+     }catch(e){
+
+     }
+ }
+
+ getDataAPI()
+
+ const showData = async(data)=>{
+     try{
+         const datos = {
+             Nombre: data.nombre,
+             email : data.email
+         }
+    
+         Object.entries(datos).forEach(dat =>{
+             console.log(dat)
+         })
+
+     }catch(e){
+         console.log(E)
+     }
+
+ }
 
 
  const deleteCom = str =>{      //Eliminar comillas 
@@ -19,36 +49,33 @@ const container = document.querySelector('.container')
 
 
 
- const getData = async()=>{
-     try{
-         const datos = await axios(url)
-         const data = datos.data
-         const search = await busqueda(data)
-         console.log(search)
-         data.forEach(data => {
-             console.log(data)
-             const divP = document.createElement('div');
-             divP.classList.add('names')
-             const elemt = {
-                 nombre: data.name,
-                 email: data.email,
-                 adress: data.city
-             }
-             let jsonTxtName = parseJson(elemt.nombre);
-             let clenTxt = deleteCom(jsonTxtName)
-             divP.textContent = clenTxt;
-             fragment.appendChild(divP);
-             container.appendChild(fragment) ;  
-         });
-
-     }catch(e){
-         console.log(e)
-
-     }
- }
+const cardFilters = document.querySelector('.card-filter');
+const card = document.querySelectorAll('.card')
+const d = document
 
 
 
+const Filters = (input)=>{
+    d.addEventListener('keyup',()=>{
+        console.log(input.value);
+
+        card.forEach(el =>{
+            let text = el.textContent.toLocaleLowerCase();
+            console.log(text)
+            const val =  text.includes(cardFilters.value)? el.classList.remove("filter") :el.classList.add("filter");
+        
+        })  
+    })
+}
+
+Filters(cardFilters)
+
+
+
+
+
+
+ 
 
  
 
